38e322abba6223ed527269e83b91cf5b5a84f2cf
khode hadi <hadi.hadi.oc@gmail.com>
2024-03-16 10:46:43 +0330

    user can submit exam one time

--- src/exam/views/front_views.py
+++ src/exam/views/front_views.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 import pytz
+from django.http import JsonResponse
 from rest_framework import viewsets, views, status
+
+from accounts.models import ChildUser
 from exam.serializers.front_serializer import *
 from rest_framework.permissions import IsAuthenticated
 from exam.permissions.permissions import *
-
+from courses.models import UserDoneContent
+from django.contrib.sessions.backends.db import SessionStore
 
 class FrontShowQuestions(views.APIView):
     permission_classes = [IsAuthenticated,IsOwner]
@@ -25,19 +29,26 @@ class FrontShowQuestions(views.APIView):
             return Response(user_course_obj.data,status=status.HTTP_400_BAD_REQUEST)
         module = course.module_rel.first()
         age = datetime.now(tz=pytz.timezone("Asia/Tehran")) - user_course_obj.created_at
-        previous_content = module.content_rel.filter(
-            Q(age__lte=age.days)
-        ).order_by("-id").first()
-        if previous_content:
+
+        session_id = request.GET.get('session')
+        session = SessionStore(session_key=session_id)
+        if session['current_user_child']!=None:
+            child_national_code = session['current_user_child']
+            child = ChildUser.objects.get(national_code=child_national_code)
+            user_done_content_obj=UserDoneContent.objects.filter(course=course,user=request.user,
+                                                  content__id=content_id,child=child)
+        if not user_done_content_obj.exists():
             contents = module.content_rel.filter(
                 Q(age__lte=age.days) & Q(content_type__model='exam') &
                 Q(pk=content_id)
             )
+            ser_data = ShowExamSerializer(instance=contents, many=True)
+            return Response(ser_data.data, status=status.HTTP_200_OK)
 
         else:
-            contents = None
-        ser_data = ShowExamSerializer(instance=contents, many=True)
-        return Response(ser_data.data, status=status.HTTP_200_OK)
+            return JsonResponse({
+                "exam_is_done":True
+            })
 
 
 class CommitExam(views.APIView):
